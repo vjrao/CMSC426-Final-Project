94cb3b3acf8cc40a06568503767f60765969190b
Vikram Rao <vikzuni@gmail.com>
2016-12-16 19:20:56 -0500

    put in basic framework for reading images

--- Code/main.m
+++ Code/main.m
@@ -1,4 +1,12 @@
 scenes = {'000'; '001'; '002'; '006'; '008'; '012'; '022'; '023'};
 % note: the frames are named frame_num_(rgb/depth).png starting from 0
 % subtract 2 for the . and .. directories and 
+
+scene = scenes{1};
 numframes = length(dir(strcat('../Data/SingleObject/scene_', scenes{2}, '/frames'))) / 2 - 2;
+for ii=0:numframes
+    % the filename "image" is specific to scene 001
+    name = strcat('../Data/SingleObject/scene_', scenes{2}, '/frames/image_', int2str(ii));
+    [Pts, colors] = RANSAC(name);
+    
+end
