c8ea82cbbec06877cbea87d8c3e5b1ecfb3fa623
Vikram Rao <vikzuni@gmail.com>
2016-12-19 17:14:54 -0500

    tmp stuff

---
+++ Code/find_edges.m
@@ -0,0 +1,6 @@
+function [Pts, colors] = find_edges (Pts, colors)
+% Given a point cloud, find all points lying on 3D edges and return those
+
+
+
+end
--- Code/main.m
+++ Code/main.m
@@ -38,19 +38,64 @@ scenes = [0, 1, 2, 6, 8, 12, 22, 23];
 % note: the frames are named frame_num_(rgb/depth).png starting from 0
 % subtract 2 for the . and .. directories and
 
-scene = 8;
+scene = 3;
 numframes = length(dir()) / 2 - 2;
 
 name = strcat(Path,'scene_',sprintf('%0.3d',scenes(scene)), '/frames/frame_', int2str(1));
 I = imread([name,'_rgb.png']);
 ID = imread([name,'_depth.png']);
 [pcx, pcy, pcz, r, g, b, D_, X, Y,validInd] = depthToCloud_full_RGB(ID, I, './params/calib_xtion.mat');
+Pts = [pcx pcy pcz];
+colors = [r g b] / 255;
+[Pts, colors] = RANSAC(Pts, colors);
+[Pts, colors] = denoise(Pts, colors);
+figure, pcshow(Pts, colors);
 
-Pts1 = [pcx pcy pcz];
-colors1 = [r g b] / 255;
-[Pts2, colors2] = RANSAC(Pts1, colors1);
-[Pts3, colors3] = denoise(Pts2, colors2);
-figure, pcshow(Pts3, colors3);
+name = strcat(Path,'scene_',sprintf('%0.3d',scenes(scene)), '/frames/frame_', int2str(2));
+I = imread([name,'_rgb.png']);
+ID = imread([name,'_depth.png']);
+[pcx, pcy, pcz, r, g, b, D_, X, Y,validInd] = depthToCloud_full_RGB(ID, I, './params/calib_xtion.mat');
+Pts2 = [pcx pcy pcz];
+colors2 = [r g b] / 255;
+[Pts2, colors2] = RANSAC(Pts2, colors2);
+[Pts2, colors2] = denoise(Pts2, colors2);
+figure, pcshow(Pts2, colors2);
+
+%{
+numpts = min([size(Pts, 1), size(Pts2, 1)]);
+Pts = Pts(1:numpts, :);
+Pts2 = Pts2(1:numpts, :);
+ptCloud = pointCloud(Pts);
+normals = pcnormals(ptCloud, 10);
+M = transf_mat(normals, Pts, Pts2);
+Pts3 = zeros(size(Pts));
+for ii=1:numpts
+    Pts3(ii,:) = M * Pts2(ii, :);
+end
+%}
+
+k = boundary(Pts);
+b1 = unique(k(:));
+bound = Pts(b1, :);
+k2 = boundary(Pts2);
+b2 = unique(k2(:));
+bound2 = Pts2(b2, :);
+idx = knnsearch(bound, bound2);
+
+
+%{
+x = ptCloud.Location(1:50:end,1);
+y = ptCloud.Location(1:50:end,2);
+z = ptCloud.Location(1:50:end,3);
+u = normals(1:50:end,1);
+v = normals(1:50:end,2);
+w = normals(1:50:end,3);
+figure, pcshow(Pts, colors); hold on;
+quiver3(x,y,z,u,v,w);
+hold off;
+%}
+
+
 
 %{
 normals = pcnormals(pointCloud(Pts));
